18c9fb73dfd0b4bd17ace6f16394e64b6c302873
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-04-23 09:01:53 +0400

    added methods
    1. shopSettingMenu
    2. manageShopItem

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class Menu {
     public static void show() {
         System.out.println("1- Shop Settings");
@@ -9,5 +11,21 @@ public class Menu {
         System.out.println("7- Program Statistics");
         System.out.println("8- Exit");
     }
+    public static void shopSettingMenu() {
+        System.out.println("Shop Settings Menu:");
+        System.out.println("1- Load Data (Items and invoices)");
+        System.out.println("2- Set Shop Name");
+        System.out.println("3- Set Invoice Header");
+        System.out.println("4- Go Back");
+    }
+    public static void manageShopItem(){
+        System.out.println("Manage Shop Items Menu:");
+        System.out.println("1- Add Item");
+        System.out.println("2- Delete Item");
+        System.out.println("3- Change Item Price");
+        System.out.println("4- Report All Items");
+        System.out.println("5- Go Back");
+    }
+
 }
 
